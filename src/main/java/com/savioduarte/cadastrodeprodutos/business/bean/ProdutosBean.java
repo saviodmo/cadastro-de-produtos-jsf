536eac985b68472be8be644f7c4ea7b3cdcf715b
savio <saviodmo@outlook.com>
2020-06-15 19:04:52 -0300

    Mostrar a tabela dos produtos cadastrados

--- src/main/java/com/savioduarte/cadastrodeprodutos/business/bean/ProdutosBean.java
+++ src/main/java/com/savioduarte/cadastrodeprodutos/business/bean/ProdutosBean.java
@@ -4,18 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 
-@RequestScoped
+@SessionScoped
 @ManagedBean(name = "produtos")
 public class ProdutosBean {
 	private List<ProdutoBean> produtos;
 	
 	public ProdutosBean() {
 		produtos = new ArrayList<>();
+		
 	}
 
+
+	
 	public List<ProdutoBean> getProdutos() {
 		return produtos;
 	}
+	
+	public void adicionar(ProdutoBean produto) {
+		produtos.add(produto);
+	}
+	
+	public void remover(ProdutoBean produto) {
+		produtos.remove(produto);
+	}
+	
+	
+	
 }
